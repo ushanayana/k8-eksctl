561f607a55081de435b2c0b3f102eaffa3bb2317
ushanayana <ushanayana@gmail.com>
2026-05-25 11:16:26 +0530

    kub8

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "sg_id" {
   type        = string
-  default     = "sg-0fea5e49e962e81c9"
+  default     = "sg-0cab6b9e7e0937b11"
   description = "description"
 }
 
 variable "public_subnet_id" {
     type = string
-    default = "subnet-0ea509ad4cba242d7"
+    default "subnet-003f9e3a9ec29d47a"
 }
